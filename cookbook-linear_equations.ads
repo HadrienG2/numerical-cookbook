18e1fdbc72ed1b04904c6802706bc9b70dd71b19
Hadrien G <knights_of_ni@gmx.com>
2015-08-12 23:39:26 +0200

    Add early interfaces for matrix LU decomposition

--- cookbook-linear_equations.ads
+++ cookbook-linear_equations.ads
@@ -13,6 +13,8 @@ package Cookbook.Linear_Equations is
    --   * Takes as input a square matrix A, and optionally a set of right-hand-side vectors Bm packed in a matrix
    --   * Stores the inverse of A in place of A, and the solution vectors Xm such that A*Xm = Bm in place of the Bm
    -- Due to the in-place nature of the algorithm, if an exception arises, the original matrices should be considered destroyed
+   --
+   -- Even if the preconditions are satisfied, the function may still throw Singular_Matrix if the input is singular.
    procedure Gauss_Jordan_Elimination (Matrix : in out F_Containers.Matrix; Right_Hand_Vectors : in out F_Containers.Matrix)
      with
        Pre => (Matrix'Length (1) = Matrix'Length (2) and then
@@ -23,6 +25,36 @@ package Cookbook.Linear_Equations is
      with
        Pre => (Matrix'Length (1) = Matrix'Length (2)),
        Post => (Matrix * Matrix'Old = F_Containers.Identity_Matrix (Matrix'Length (1)));
+
+   -- LU decompositions are stored inside an opaque object, which allows for some storage optimizations.
+   -- Components are indirectly available through member functions.
+   type LU_Decomposition(<>) is private
+     with
+       Type_Invariant => Lower (LU_Decomposition) * Upper (LU_Decomposition) = Original_Matrix (LU_Decomposition);
+   function Matrix_Size (LU : LU_Decomposition) return Size_Type;
+   function Lower (LU : LU_Decomposition) return F_Containers.Matrix
+     with
+       Post => (Lower'Result'Length (1) = Lower'Result'Length (2) and then
+                      Lower'Result'Length (1) = Matrix_Size (LU));
+   function Upper (LU : LU_Decomposition) return F_Containers.Matrix
+     with
+       Post => (Upper'Result'Length (1) = Upper'Result'Length (2) and then
+                      Upper'Result'Length (1) = Matrix_Size (LU));
+   function Original_Matrix (LU : LU_Decomposition) return F_Containers.Matrix
+     with
+       Post => (Original_Matrix'Result'Length (1) = Original_Matrix'Result'Length (2) and then
+                      Original_Matrix'Result'Length (1) = Matrix_Size (LU));
+
+   -- LU decomposition may be computed using Crout's algorithm, and opens many possibilities for efficient computations on the original matrix
+   --
+   -- As before, the algorithm may throw Singular_Matrix even if all preconditions are satisfied if the input is singular.
+   function Crout_LU_Decomposition (Matrix : F_Containers.Matrix) return LU_Decomposition
+     with
+       Pre => (Matrix'Length (1) = Matrix'Length (2)),
+       Post => (Matrix_Size (Crout_LU_Decomposition'Result) = Matrix'Length (1) and then
+                      Lower (Crout_LU_Decomposition'Result) * Upper (Crout_LU_Decomposition'Result) = Matrix);
+   -- TODO : Add RHS solver, inverse computation, determinant, and all that jazz, with appropriate pre/postconditions
+
 
    -- Test the functions within this package
    procedure Test;
