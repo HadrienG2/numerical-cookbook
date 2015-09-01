0054af326a065f69848a256cfa79c4f19730d84b
Hadrien <knights_of_ni@gmx.com>
2015-09-01 15:00:05 +0200

    Clarify that Identity_Matrix only works for nonzero matrix sizes

--- cookbook-generic_containers.ads
+++ cookbook-generic_containers.ads
@@ -45,6 +45,7 @@ package Cookbook.Generic_Containers is
    -- The identity matrix is often useful, so we'll provide a cheap function to generate it
    function Identity_Matrix (Size : Size_Type) return Matrix
      with
+       Pre => (Size > 0),
        Post => (Identity_Matrix'Result'Length (1) = Identity_Matrix'Result'Length (2) and then
                       Identity_Matrix'Result'Length (1) = Size);
 
