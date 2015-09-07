27e41f7980ea067aec482d903b23e57ce0d7f874
Hadrien G <knights_of_ni@gmx.com>
2015-09-07 23:17:39 +0200

    Testing is over, putting everything back in working order...

--- main.adb
+++ main.adb
@@ -19,10 +19,10 @@
 with Cookbook;
 with Cookbook.Float_Containers;
 with Cookbook.Float_Utility;
--- with Cookbook.Linear_Equations;
--- with Cookbook.Linear_Equations.Gauss_Jordan;
--- with Cookbook.Linear_Equations.LU_Decomp;
--- with Cookbook.Linear_Equations.Tridiagonal;
+with Cookbook.Linear_Equations;
+with Cookbook.Linear_Equations.Gauss_Jordan;
+with Cookbook.Linear_Equations.LU_Decomp;
+with Cookbook.Linear_Equations.Tridiagonal;
 
 procedure Main is
    -- We take float equality as equality up to a difference of one significant digit for nonzero numbers,
@@ -41,13 +41,13 @@ procedure Main is
 
    -- Instantiate the whole cookbook
    package Float_Cookbook is new Cookbook (Float_Type => Float, "=" => Relatively_Equal, Index_Type => Positive, Size_Type => Natural);
-   package Float_Utility is new Float_Cookbook.Float_Utility with Unreferenced;
-   package Float_Containers is new Float_Cookbook.Float_Containers with Unreferenced;
-   -- package Linear_Equations is new Float_Cookbook.Linear_Equations (F_Containers => Float_Containers,
-   --                                                                  F_Utility => Float_Utility);
-   -- package Gauss_Jordan is new Linear_Equations.Gauss_Jordan with Unreferenced;
-   -- package LU_Decomp is new Linear_Equations.LU_Decomp with Unreferenced;
-   -- package Tridiagonal is new Linear_Equations.Tridiagonal with Unreferenced;
+   package Float_Utility is new Float_Cookbook.Float_Utility;
+   package Float_Containers is new Float_Cookbook.Float_Containers;
+   package Linear_Equations is new Float_Cookbook.Linear_Equations (F_Containers => Float_Containers,
+                                                                    F_Utility => Float_Utility);
+   package Gauss_Jordan is new Linear_Equations.Gauss_Jordan with Unreferenced;
+   package LU_Decomp is new Linear_Equations.LU_Decomp with Unreferenced;
+   package Tridiagonal is new Linear_Equations.Tridiagonal with Unreferenced;
 begin
    null;
 end Main;
